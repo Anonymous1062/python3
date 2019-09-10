461792c09192e36b0de3d2f97f7f2b14f086b109
Jacques Lamé <jacques.lame@coderclass.nl>
2019-09-10 15:43:29 +0200

    Readfile and addtofile

--- eindopdracht.py
+++ eindopdracht.py
@@ -1,1 +1,30 @@
-file = open("hellothere.txt", "a+")
+def readfile():
+    with open('hellothere.txt') as f:
+      bestandsdata = f.read().split('\n')
+
+    dictionary = {}
+
+    for item in bestandsdata:
+        if not item == '':
+            woord1, woord2 = item.split("=")
+            dictionary[woord1] = woord2
+    return dictionary
+
+def addtofile(newkey, newvalue):
+    with open('hellothere.txt', 'a') as f:
+        f.write(newkey + "=" + newvalue + "\n")
+
+
+'''
+def removefromfile(removedkey):
+    dictionary = readfile()
+    for i in range(len(dictionary)):
+        if removedkey == dictionary[i]:
+            print(dictionary[i])
+    with open("memes.txt", "a") as b:
+        regel = int(input("Welke regel wilt u verwijderen? : "))
+        regel = regel - 1
+        del line[regel]
+removefromfile("Sheev Palpatine")
+
+'''

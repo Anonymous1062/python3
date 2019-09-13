24b55327a336c8f9546f3241123a571bc1f02ece
Jacques Lamé <jacques.lame@coderclass.nl>
2019-09-13 11:43:35 +0200

    Finished Python 3

--- eindopdracht.py
+++ eindopdracht.py
@@ -15,16 +15,40 @@ def addtofile(newkey, newvalue):
         f.write(newkey + "=" + newvalue + "\n")
 
 
-'''
 def removefromfile(removedkey):
     dictionary = readfile()
-    for i in range(len(dictionary)):
-        if removedkey == dictionary[i]:
-            print(dictionary[i])
-    with open("memes.txt", "a") as b:
-        regel = int(input("Welke regel wilt u verwijderen? : "))
-        regel = regel - 1
-        del line[regel]
-removefromfile("Sheev Palpatine")
+    if removedkey in dictionary:
+        del dictionary[removedkey]
+        with open('hellothere.txt', 'w') as f:
+            for item in dictionary:
+                f.write(item + "=" + dictionary[item] + "\n")
+        return "Successfully deleted"
+    else:
+        return "Impossible. Perhaps the archives are incomplete"
 
-'''
+def menu():
+    print('''
+If you want to see the contents of the file, say read
+If you want to add something to the file, say write
+If you want to delete something from the file, say remove
+If you want to stop the program, say cancel\n''')
+    choice = input("Pick one of the options: ")
+    if choice == "read":
+        print(readfile())
+        menu()
+    elif choice == "write":
+        addkey = input("What will the new key be? ")
+        addvalue = input("What will the new value be? ")
+        addtofile(addkey, addvalue)
+        print("Adding to file complete")
+        menu()
+    elif choice == "remove":
+        delkey = input("Which key do you want to remove from the file? ")
+        print(removefromfile(delkey))
+        menu()
+    elif choice == "cancel":
+        print("Program terminated")
+    else:
+        menu()
+print("Hello there!")
+menu()
